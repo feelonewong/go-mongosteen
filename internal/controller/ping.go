f1a72163658c1534b060d0f42ba8418d2b8aa3c2
feelonewong <feelonewong@gmail.com>
2023-04-12 23:18:02 +0800

    update: 完成ping的单元测试

--- internal/controller/ping.go
+++ internal/controller/ping.go
@@ -3,5 +3,9 @@ package controller
 import "github.com/gin-gonic/gin"
 
 func PingHandle(c *gin.Context) {
+	// c.Request.xxx 读取请求相关
+	// c.JSON() 返回响应相关
+	// 抽离controller好处
+	// 1.减少router的体积 2.方便进行单元测试
 	c.Writer.WriteString("Hello World")
 }
--- test/controller_test/ping_test.go
+++ test/controller_test/ping_test.go
@@ -1,1 +1,18 @@
 package controller_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"go-mongosteen/internal/router"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPingHandle(t *testing.T) {
+	r := router.New()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	r.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "Hello World", w.Body.String())
+}
